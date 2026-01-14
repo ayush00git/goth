63e699c000bebc9ef008b9bd1013a37b62cc36f5
ayush00git <ayush.2007.iit@gmail.com>
2026-01-14 21:29:59 +0530

    basic handlers and models logic updated

---
+++ internals/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID			primitive.ObjectID		`json:"id" bson:"id, omitempty"`
+	Email		string					`json:"email" bson:"email"`
+	Password	string					`json:"password" bson:"password"`
+	CreatedAt	time.Time				`json:"created_at" bson:"created_at"`
+};

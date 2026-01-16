691e3148dc8c195f9319367d75ae8177681a5d1e
ayush00git <ayush.2007.iit@gmail.com>
2026-01-16 16:12:51 +0530

    feat: added blogs model

---
+++ internals/models/blogs.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+)
+
+type Blog struct {
+	ID			primitive.ObjectID		`json:"id" bson:"_id, omitempty"`
+	Title		string					`json:"title" bson:"title"`
+	Excerpt		string					`json:"excerpt" bson:"excerpt"`
+	Tags		[]string				`json:"tags" bson:"tags"`
+	Body		string					`json:"content" bson:"content"`
+	IsDraft		bool					`json:"isDraft" bson:"isDraft"`
+	CreatedAt	time.Time				`json:"created_at" bson:"created_at"`
+}

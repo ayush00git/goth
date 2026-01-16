ce4e9bbe0f153f91850bbfc97a5348e75d05c0d8
ayush00git <ayush.2007.iit@gmail.com>
2026-01-16 17:46:31 +0530

    blogs routes setup

---
+++ internals/routes/blogs.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"goth/internals/handlers"
+	"net/http"
+)
+
+func Blog(blogHandler *handlers.BlogHandler) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /blog/write", blogHandler.WriteBlog)
+	mux.HandleFunc("GET /blog/", blogHandler.GetBlog)
+
+	return mux
+}

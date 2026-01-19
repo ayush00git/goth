cd53bae22ffcc115d3203bca0a432b1a60b21480
ayush00git <ayush.2007.iit@gmail.com>
2026-01-19 19:42:18 +0530

    fix(routes): clean routes logic

--- internals/routes/auth.go
+++ internals/routes/auth.go
@@ -8,9 +8,9 @@ import (
 func Auth (authHandler *handlers.AuthHandler) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /auth/signup", authHandler.Signup)
-	mux.HandleFunc("POST /auth/login", authHandler.Login)
-	mux.HandleFunc("GET /auth/users", authHandler.GetUsers)
+	mux.HandleFunc("POST /auth/signup/", authHandler.Signup)
+	mux.HandleFunc("POST /auth/login/", authHandler.Login)
+	mux.HandleFunc("GET /auth/users/", authHandler.GetUsers)
 
 	return mux
 }

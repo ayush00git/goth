886dbeff3438f3d57546bf7b6b14ae987c6b6976
ayush00git <ayush.2007.iit@gmail.com>
2026-01-22 22:12:13 +0530

    added author id ref

--- internals/models/blogs.go
+++ internals/models/blogs.go
@@ -7,11 +7,13 @@ import (
 )
 
 type Blog struct {
-	ID			primitive.ObjectID		`json:"id" bson:"_id, omitempty"`
-	Title		string					`json:"title" bson:"title"`
-	Excerpt		string					`json:"excerpt" bson:"excerpt"`
-	Tags		[]string				`json:"tags" bson:"tags"`
-	Content		string					`json:"content" bson:"content"`
-	IsDraft		bool					`json:"isDraft" bson:"isDraft"`
-	CreatedAt	time.Time				`json:"created_at" bson:"created_at"`
+	ID				primitive.ObjectID		`json:"id" bson:"_id, omitempty"`
+	AuthorID	primitive.ObjectID			`json:"author_id" bson: "author_id, omitempty"`
+	Title			string								`json:"title" bson:"title"`
+	Excerpt		string								`json:"excerpt" bson:"excerpt"`
+	Tags			[]string							`json:"tags" bson:"tags"`
+	Content		string								`json:"content" bson:"content"`
+	IsDraft		bool									`json:"isDraft" bson:"isDraft"`
+	CreatedAt	time.Time							`json:"created_at" bson:"created_at"`
 }
+

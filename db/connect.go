2666b6f8be16e4b98db21c9de36d5338f21f66fb
ayush00git <ayush.2007.iit@gmail.com>
2026-02-14 22:52:22 +0530

    fix(db): monogdb connection moved from main to db package

---
+++ db/connect.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"time"
+	"log"
+	"fmt"
+
+	"goth/helpers"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongoDB (collection string) *mongo.Collection {
+	uri := helpers.GetEnvVar("MONGO_URI")
+
+	// context, client, ping
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Connection to MongoDB established")
+
+	collec := client.Database("goth").Collection(collection)
+	return collec 
+}
--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"goth/handlers"
 	"goth/routes"
 	"goth/db"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 
-	// getting MONGO_URI from env and connecting
-	// to the database
-	if err := godotenv.Load(); err != nil {
-		log.Fatal(`Unable to locate the .env`)
-	}
-
-	uri := os.Getenv("MONGO_URI")
-	if uri == "" {
-		log.Fatal("MONGO_URI string is not present in the env variables")
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := client.Ping(ctx, nil); err != nil {
-		log.Fatal("Connection to MongoDB is failing")
-	}
-	fmt.Println("MongoDB connected successfully!")
+	AuthCollection := db.ConnectMongoDB("users")
 
 	// Defining the handlers and routes
-	AuthCollection := client.Database("goth_db").Collection("users")
 	db.CreateIndex(AuthCollection)
 
 	authHandler := &handlers.AuthHandler{

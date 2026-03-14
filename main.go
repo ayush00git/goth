4a0e05e088291ca3c9682bde28b3d16c4481a9e2
Ayush Kumar <ayush.2007.iit@gmail.com>
2026-03-14 22:04:27 +0530

    feat(grpc): all grpc streaming patterns added (#23)
    * feat(grpc): .proto now covers all streaming patterns
    
    * feat(grpc): added grpc server signup and login methods
    
    * feat(grpc): added the remining rpc methods
    
    * feat(grpc): added grpc streaming methods

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"log"
+	"net"
 
 	"github.com/ayush00git/goth/db"
+	grpcserver "github.com/ayush00git/goth/grpc"
 	"github.com/ayush00git/goth/handlers"
+	pb "github.com/ayush00git/goth/proto"
 	"github.com/ayush00git/goth/routes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,6 +29,24 @@ func main() {
 	r := gin.Default()
 	routes.AuthRoute(r, authHandler)
 
-	log.Println("Server running on port 8080")
-	log.Fatal(r.Run(":8080"))
+	go func() {
+		log.Println("Server running on port 8080")
+		log.Fatal(r.Run(":8080"))
+	} ()
+
+	// grpc server
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal("failed to listed, err: %v", err)
+	}
+
+	grpcSrv := grpc.NewServer()
+
+	pb.RegisterAuthServiceServer(grpcSrv, &grpcserver.AuthGRPCServer{
+		Collection: AuthCollection,
+	})
+
+	reflection.Register(grpcSrv)
+	log.Println("gRPC server running on :50051")
+	log.Fatal(grpcSrv.Serve(lis))
 }

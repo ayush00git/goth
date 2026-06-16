2997f1e6b1680fae09a2c298e907eae0c66f8f00
Ayush Kumar <ayush.2007.iit@gmail.com>
2026-06-16 23:30:08 +0530

    feat: register signup rpc handler using a pgxpool instance (#28)
    Registered a Signup rpc handler + `GothServer` struct now hold the `pgxpool` instance which accounts for shared db thread connections for any rpc handlers.

---
+++ internal/handlers/goth.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/ayush00git/goth/grpc/goth"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// embeds the generated grpc server implementation for forward compatibility.
+// shared psql connection pools to be used by rpc handlers.
+type GothServer struct {
+	goth.UnimplementedGothServiceServer
+	db *pgxpool.Pool
+}
+
+// NewServer creates a new gRPC sever with its required dependencies.
+func NewServer(db *pgxpool.Pool) *GothServer {
+	return &GothServer{
+		db: db,
+	}
+}
+
+
+func (g *GothServer) Signup(ctx context.Context, req *goth.SignupRequest) (*goth.SignupResponse, error) {
+	// inputs from request body
+	email := req.Email
+	password := req.Password
+	fullName := req.FullName
+
+	// validate the requests
+	if email == "" || password == "" || fullName == "" {
+		return nil, status.Error(codes.InvalidArgument, "email, full_name and password are required fields")
+	}
+
+	// hash the password
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to hash the password")
+	}
+
+	// write to db
+	var userID string
+	err = g.db.QueryRow(
+		ctx,
+		`INSERT INTO USERS (email, password_hash, full_name)
+		VALUES ($1, $2, $3)`,
+		email,
+		hashedPass,
+		fullName,
+	).Scan(&userID)
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to create the user")
+	}
+
+	// return the SignupResponse struct
+	return &goth.SignupResponse{
+		UserId: 		userID,
+		Email: 			email,
+		EmailVerified:  false,
+	}, nil
+}
